82321ed0ced599d2b9a2c8b6fa5b978ee5dd1b05
mitshel <mitshel@mail.ru>
2013-08-05 17:06:06 +0400

    Work with XML-BlackBox functional

--- XML/SQLQuery1.sql
+++ XML/SQLQuery1.sql
@@ -1,12 +1,17 @@
 create table #t (id int identity, tname varchar(20), data xml)
 go
 
-insert into #t(tname, data) values('tblObjects','<id code="1"><before><name>123</name></before><after><name>456</name></after></id>')
+insert into #t(tname, data) values('tblObjects','<id code="1"><before code="123"><name>123</name></before><after code="345"><name>456</name></after></id>')
 go
 
 select * from #t
 go
 
-
+Select data.query('/id[@code="1"]/before/name/text()') from #t
+go
+Select nref.value('@code','varchar(max)') from #t 
+CROSS APPLY data.nodes('//before') AS R(nref) 
+go
 
 drop table #t
+
